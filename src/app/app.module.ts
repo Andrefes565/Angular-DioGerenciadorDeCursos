e11cff62065e166ce00a6e26c407fec8d179c858
Andrefes565 <andrexfes07@gmail.com>
2021-07-28 13:56:50 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourselistComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipes/replace.pipe';
+import { NavBarComponent } from './courses/nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourselistComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourselistComponent
+      },
+      {
+        path: '**', component: Error404Component // ** é para quando ele não encontrar a pagina/link
+      }
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
